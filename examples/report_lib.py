ab6c0494175d844f5802505eeab8ffcbe3039c32
Dennis Box <dbox@fnal.gov>
2018-11-27 12:48:05 -0600

    example running a bunch of tests

---
+++ examples/report_lib.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+#constants
+
+RED='#ff0000'
+GREEN = '#00ff00'
+BLUE = '#0000ff'
+BLACK = 'solid black'
+BL = 'border-left'
+BR = 'border-right'
+BC = 'background-collapse: collapse'
+TEC = 'text-align: center'
+BOLD = 'font-weight: bold'
+
+HEAD_FMT="""
+<html>
+<head></head>
+<h3>CI build of GlideinWMS_CI workflow for sl{release} Succeeded</h3> 
+<br>Build number: {build_number}
+<br>GlideinwmsCI Web App: <a href={gwms_ci_url}>here</a>
+<br>Jenkins build: <a href={jenkins_ci_url}>here</a>
+<br> <b>HOSTNAME:</b> {hostname} <br/>
+<b>LINUX DISTRO:</b>{distro}<br/>
+<b>PYTHON LOCATION:</b>{py_loc}<br/>
+<b>PYLINT:</b>{pylint_ver}<br/>
+<b>PEP8:</b> {pep8_ver}<br/>
+</p><table style="border: 1px solid black;{bc};">
+<thead style="{bold};border: 0px solid black;background-color: #ffcc00;">
+<tr style="padding: 5px;text-align: center;">
+<th rowspan="2" style="background-color:  #757bff;padding: 8px;text-align: center;">GIT BRANCHES</th>
+<th colspan=4 style="{bl}: 1px solid black;{br}:1px solid black;{bc};{bold};background-color: #757bff;padding: 8px;"> <b>PYLINT</b> </th>
+<th colspan=4 style="{bl}: 1px solid black;{br}:1px solid black;{bc};{bold};background-color: #757bff;padding: 8px;"> <b>UNIT TESTS</b> </th>
+<th colspan=1 style="{bl}: 1px solid black;{br}:1px solid black;{bc};{bold};background-color: #757bff;padding: 8px;"> <b>FUTURIZE</b> </th>
+<tr style="padding: 5px;text-align: center;">
+       <!--pylint-->
+      <th style="{bl}:1px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">FILES CHECKED</th>
+      <th style="{bl}:0px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">FILES WITH ERRORS</th>
+      <th style="{bl}:0px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">TOTAL ERRORS</th>
+      <th style="{bl}:0px solid black; {br}:1px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">PEP8 ERRORS</th>
+      <!--unit tests-->
+      <th style="{bl}:1px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">TESTS</th>
+      <th style="{bl}:0px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">SKIPPED</th>
+      <th style="{bl}:0px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">FAILED</th>
+      <th style="{bl}:0px solid black; {br}:0px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">% COVERAGE</th>
+      <!--futurize-->
+      <th style="{bl}:1px solid black; {br}:1px solid black;{bc};{bold};background-color: #ffb300;padding: 8px;">FILES TO BE REFACTORED</th>
+</tr> 
+</thead>
+
+"""
+
+
+ROW_FMT = """
+<tr style="padding: 5px;{tec};">
+    <!--pylint-->
+    <th style="{bl}:1px {black}; {br}:0px {black};{bc};{bold};background-color:#00ccff;padding: 8px;">{branch_name}</th>
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {green};padding: 5px;{tec};">{files_checked}</td>
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {pnt_err_color};padding: 5px;{tec};">{pylint_error_files}</td>
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {pnt_err_color};padding: 5px;{tec};">{total_errors}</td>
+    <td style="torder-left:0px {black}; {br}:1px {black};{bc};background-color: {pnt_err_color};padding: 5px;{tec};">{pep8_errors}
+    <!--unit tests-->
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {green};padding: 5px;{tec};">{num_unit_tests}</td> 
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {green};padding: 5px;{tec};">{skipped}</td>
+    <td style="{bl}:0px {black}; {br}:0px {black};{bc};background-color: {ut_err_color};padding: 5px;{tec};">{unit_test_errors}</td>
+    <td style="torder-left:0px {black}; {br}:1px {black};{bc};background-color: {green};padding: 5px;{tec};">{coverage}</td>
+    <!--futurize-->
+    <td style="{bl}:1px {black}; {br}:1px {black};{bc};background-color: {refact_color};padding: 5px;{tec};">{num_to_refactor}</td>
+</tr>
+"""
+
+TAIL_FMT = """
+</table></body>
+</html>
+"""
+def get_color(acol):
+    red = '#ff0000'
+    green = '#00ff00'
+    acol = int(acol)
+    if acol:
+        return red
+    return green
+
+
+def print_head(rh_rel=7, build_number=0, gwms_ci_url='burp',
+               jenkins_ci_url='burp', hostname='container', distro='burp',
+               py_loc='virtualenv', pylint_ver='burp', pep8_ver='burp'):
+    print(HEAD_FMT.format(bold=BOLD,
+                          bl=BL,
+                          br=BR,
+                          bc=BC,
+                          release=rh_rel,
+                          build_number=build_number,
+                          gwms_ci_url=gwms_ci_url,
+                          jenkins_ci_url=jenkins_ci_url,
+                          hostname=hostname,
+                          distro=distro,
+                          py_loc=py_loc,
+                          pylint_ver=pylint_ver,
+                          pep8_ver=pep8_ver))
+                          
+def print_tail():
+    print(TAIL_FMT)
+
+def print_row(branch_name="master", file_check_cnt=0, num_pylint_err_files=0,
+              num_pylint_errs=0, num_pep8_errs=0, num_unit_tests=0,
+              num_unit_test_skipped=0, num_unit_test_errors=0, coverage='0%',
+              num_to_refactor=0):
+
+    print(ROW_FMT.format(red=RED,
+                         green=GREEN,
+                         blue=BLUE,
+                         black=BLACK,
+                         bold=BOLD,
+                         bl=BL,
+                         br=BR ,
+                         bc=BC,
+                         tec=TEC,
+                         branch_name = branch_name,
+                         files_checked = file_check_cnt,
+                         pylint_error_files=num_pylint_err_files,
+                         pnt_err_color = get_color(num_pylint_err_files),
+                         total_errors = num_pylint_errs,
+                         pep8_errors = num_pep8_errs,
+                         num_unit_tests = num_unit_tests,
+                         skipped = num_unit_test_skipped,
+                         unit_test_errors = num_unit_test_errors,
+                         ut_err_color=get_color(num_unit_test_errors),
+                         coverage=coverage,
+                         num_to_refactor = num_to_refactor,
+                         refact_color = get_color(num_to_refactor)))
+    
+
+
+if __name__ == '__main__':
+    print_head()
+    print_row()
+    print_tail()
---
+++ examples/run_tests.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+if [ "$IMAGE" = "" ]; then
+    IMAGE=dbox/gwms-test
+fi
+
+if [ "$1" = "" ]; then
+    DAYS=14
+else
+    DAYS=$1
+fi
+
+if [ -e "$HOME/docker" ]; then
+   docker=${HOME}/docker
+   DOCKER="$docker  -H tcp://131.225.67.229:2375"
+elif [ -e "$(which docker)" ] ; then
+   docker=$(which docker)
+   DOCKER="$docker "
+else
+   echo docker not found
+   exit 1
+fi
+
+
+$DOCKER pull $IMAGE
+$DOCKER pull $IMAGE:sl6
+$DOCKER pull $IMAGE:sl7
+
+if [ "$SAVE_OLD_CONTAINERS_1" = "" ]; then
+    for C in $($DOCKER ps -a | grep v3  | awk '{print $1}'); do $DOCKER rm -f "$C"; done
+fi
+
+BRANCHES=$($DOCKER run --rm $IMAGE new_branches "$DAYS")
+CLIST=""
+PERSIST="while [ 0 -eq 0 ]; do date; sleep 60; done;"
+for rh in 6 7; do
+    for branch in $BRANCHES; do 
+        container=$(echo "$branch"| sed -e's/\//_/g' -e's/-/_/g')_sl$rh;  
+        CLIST="$CLIST $container"
+        if $DOCKER run -d --name "$container"  $IMAGE:sl$rh /bin/bash -c "$PERSIST" ; then
+            echo "started containter $container"
+        else
+            echo "failed to start $container in [ $CLIST ] " 
+            echo "project branch $branch" 
+            exit 1
+        fi
+    done
+done
+echo "running jobs in the following containters:"
+echo "$CLIST"
+
+for rh in 6 7; do
+    for branch in $BRANCHES; do
+        container=$(echo "$branch"| sed -e's/\//_/g' -e's/-/_/g')_sl$rh;
+        $DOCKER exec  -i "$container" setup_test_env  "$branch"
+        $DOCKER exec -d "$container" run_unit_tests "$branch"
+        $DOCKER exec -d "$container" run_pylint "$branch"
+        $DOCKER exec -d "$container" run_futurize "$branch"
+    done
+done
+
+#wait for jobs to finish
+#PS_CMD='ps auxww | grep -iE "run_pylint|run_unit_tests|run_futurize" | grep local | grep -v grep'
+PS_CMD='ps auxww | grep \/usr\/local\/bin | grep -v grep'
+RUNNING=$(for C in $CLIST; do $DOCKER exec -i "$C" /bin/bash -c "$PS_CMD"; done | wc -l)
+
+while [ "$RUNNING" -ne  0 ]; do
+    echo "waiting on $RUNNING processes"
+    date
+    sleep 60
+    RUNNING=$(for C in $CLIST; do $DOCKER exec -i "$C" /bin/bash -c "$PS_CMD"; done | wc -l)
+done
+
+#copy all the results back
+#rm -rf output_6 output_7
+test -d output_7 && mv output_7 "output_7.$(date +%s)"
+test -d output_6 && mv output_6 "output_6.$(date +%s)"
+mkdir -p output_6/output output_7/output
+
+for C in $CLIST; do
+    if [ "$(echo "$C" | grep sl7)" = "" ]; then
+        #echo $DOCKER cp "$C":/test_dir/output output_6  
+        $DOCKER cp "$C":/test_dir/output output_6
+    else
+        #echo $DOCKER cp "$C":/test_dir/output output_7 
+        $DOCKER cp "$C":/test_dir/output output_7
+    fi
+done
+
+
+if [ "$SAVE_OLD_CONTAINERS_2" = "" ]; then
+    for C in $CLIST; do $DOCKER kill "$C" ; $DOCKER rm "$C"; done
+fi
+
+if [ "$WORKSPACE/$BUILD_NUMBER" != "/" ]; then
+    mkdir -p $WORKSPACE/$BUILD_NUMBER
+    mv output_7 $WORKSPACE/$BUILD_NUMBER/sl7
+    mv output_6 $WORKSPACE/$BUILD_NUMBER/sl6
+fi
+
