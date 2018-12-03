d6405e7e665cf93355fe04075f271fcd21a10ad1
Dennis Box <dbox@fnal.gov>
2018-12-03 14:26:54 -0600

    harden report, include coverage pct

--- examples/make_report.sh
+++ examples/make_report.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
+function usage {
+echo "usage: $0 /path/to/results/directory"
+echo "merge pep8, pylint, coverage, and futurize test output "
+echo "create html reports pointed to by \$html_test_report, \$html_coverage_browser"
+echo "also create text report \$outfile all defined below"
+echo "TODO assumes that tail directory in /path/to/etc is BUILD_NUMBER"
+echo "which is a jenkins artifact"
+exit 1
+}
+
+if [ "$1" = "" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+fi
+
 PATHTO=../$(dirname $0)
 
 export BUILD_NUMBER=$1
-cd $BUILD_NUMBER
-
-$PATHTO/print_header.py > report.html
 
 outfile=test_"$BUILD_NUMBER"_results.txt
+html_test_report="$BUILD_NUMBER"_results.html
+html_coverage_browser="$BUILD_NUMBER"_coverage_browser.html
+
+cd $BUILD_NUMBER || usage
+
+$PATHTO/print_header.py > $html_test_report
+
+
 echo "test $BUILD_NUMBER on $(date)" > $outfile
-for dir in $(find .  -type d -name xml_reports); 
+for dir in $(find .  -type d -name xml_reports); do
     export branch=$(basename $(dirname $dir))
     export arch=$(basename $(dirname $(dirname $(dirname $dir))))
     export arch_branch="$branch - $arch"
@@ -35,12 +54,12 @@ for dir in $(find .  -type d -name xml_reports);
     export error_count=$sum
 
     export skipped=$(grep skipped $dir/*.xml | wc -l)
-    coverage=$(tail -1 $dir../coverage.report.* | awk '{print $4}')
+    coverage=$(tail -1 $(find $dir/../ -name 'coverage.report.*')| awk '{print $4}')
     export coverage
     for x in $(tail -4  $dir/../results.log); do  eval $x ; done
     export FILES_CHECKED_COUNT PYLINT_ERROR_FILES_COUNT PYLINT_ERROR_COUNT PEP8_ERROR_COUNT
     export branch="$arch_branch"
-    $PATHTO/print_row.py >> report.html
+    $PATHTO/print_row.py >> $html_test_report
     echo '' >> $outfile
     echo branch=$branch >>$outfile
     echo num_tests=$num_tests error_count=$error_count skipped=$skipped tot_time=$tot_time>>$outfile
@@ -48,13 +67,14 @@ for dir in $(find .  -type d -name xml_reports);
     echo PYLINT_ERROR_COUNT=$PYLINT_ERROR_COUNT PEP8_ERROR_COUNT=$PEP8_ERROR_COUNT>>$outfile
     echo '' >> $outfile
 done
-echo "</table></body></html>" >> report.html
-outfile='coverage_report.html'
-echo "<html><head>coverage report build $BUILD_NUMBER</head><body><h1>Build $BUILD_NUMBER Coverage by Branch</h1>" >$outfile
+echo "</table></body></html>" >> $html_test_report
+
+#link all the html coverage reports together on page $html_coverage_browser
+echo "<html><head>coverage report build $BUILD_NUMBER</head><body><h1>Build $BUILD_NUMBER Coverage by Branch</h1>" >$html_coverage_browser
 for L in $(find . -name index.html); do 
     pct=$(grep pc_cov $L| sed -e 's/.*">//' -e's/<.*//') 
     branch=$(echo $L | sed -e 's/.*output\///' -e 's/\/htmlcov.*//') 
     arch=$(echo $L | sed -e 's/\/output.*//' -e 's/\.\///')
-    echo "<li><a href=\"$L\"> $arch $branch $pct</a>" >> $outfile 
+    echo "<li><a href=\"$L\"> $arch $branch $pct</a>" >> $html_coverage_browser
 done
-echo "</body></html>" >> $outfile
+echo "</body></html>" >> $html_coverage_browser
